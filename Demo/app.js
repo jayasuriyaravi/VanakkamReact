f2c22137eb8d3839224f585801175063d615e776
jayasuriyaravi19@gmail.com <yourgithubemail@example.com>
2025-08-21 17:21:09 +0530

    adding

--- Demo/app.js
+++ Demo/app.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // ✅ updated import
+
 const content = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "first" }, "this is first box"),
-    React.createElement("div", { id: "sec" }, "this is first box")
+    [React.createElement("div", { id: "first", key: "first" }, "this is first box"),
+        React.createElement("div", { id: "sec", key: "secound" }, "this is first box")
     ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // creating the root to displaying all the element into the react dom
